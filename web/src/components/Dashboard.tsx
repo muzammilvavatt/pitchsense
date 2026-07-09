1fbb0051e2ba88e728edaf5f84f5ec1866dc1472
Muzammil <vavathouse@gmail.com>
2026-07-09 10:44:22 +0530

    Import useEffect in Dashboard

--- web/src/components/Dashboard.tsx
+++ web/src/components/Dashboard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LogOut, Trophy, MessageSquare, LayoutDashboard } from "lucide-react";
 import MatchHub from "./MatchHub";
 import DebateFeed from "./DebateFeed";
